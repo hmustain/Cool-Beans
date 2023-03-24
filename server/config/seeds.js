013ab4644125208ef5aabb37671a8ac129f7b72a
hmustain <hunter@bunkerbranding.com>
2023-03-24 08:42:40 -0500

    adding users to seed file

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,12 +1,40 @@
-const db = require('./connection');
-const { User } = require('../models');
+const db = require("./connection");
+const { User } = require("../models");
 
 const userSeedData = [
-    {
-        firstName: 'Caleb',
-        lastName: 'Carnett',
-        email: 'caleb@example.com',
-        password: 'Password12345!',
-        role: 'admin'
-    },
-]
+  {
+    firstName: "Caleb",
+    lastName: "Carnett",
+    email: "caleb@example.com",
+    password: "Password12345!",
+    role: "admin",
+  },
+
+  {
+    firstName: "Kaikane",
+    lastName: "Lacno",
+    email: "kai@example.com",
+    password: "Password12345!",
+    role: "admin",
+  },
+
+  {
+    firstName: "Hunter",
+    lastName: "Mustain",
+    email: "hunter@example.com",
+    password: "Password12345!",
+    role: "admin",
+  },
+];
+
+const seedUsers = async () => {
+  try {
+    await User.deleteMany({});
+    await User.create(userSeedData);
+    console.log("Users seeded successfully!");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = seedUsers;
