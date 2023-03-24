4e74680b3a65c28172245a9c8b44c206b72f4716
hmustain <hunter@bunkerbranding.com>
2023-03-24 13:44:33 -0500

    adding seed data for products

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
 const db = require("./connection");
-const { User, Collection } = require("../models");
+const { User, Collection, Product } = require("../models");
 
 const userSeedData = [
   {
@@ -39,6 +39,82 @@ const collectionSeedData = [
   },
 ];
 
+const productSeedData = [
+  {
+    name: "Light Roast 1",
+    description: "This is a sample product",
+    image: "/client/images/light-roast-coffee.jpg",
+    category: "LightRoast",
+    price: 14.99,
+    quantity: 500
+  },
+  {
+    name: "Light Roast 2",
+    description: "This is a sample product",
+    image: "/client/images/light-roast-coffee.jpg",
+    category: "LightRoast",
+    price: 14.99,
+    quantity: 500
+  },
+  {
+    name: "Light Roast 3",
+    description: "This is a sample product",
+    image: "/client/images/light-roast-coffee.jpg",
+    category: "LightRoast",
+    price: 14.99,
+    quantity: 500
+  },
+  {
+    name: "Medium Roast 1",
+    description: "This is a sample product",
+    image: "/client/images/medium-roast-coffee.jpg",
+    category: "mediumRoast",
+    price: 15.99,
+    quantity: 500
+  },
+  {
+    name: "Medium Roast 2",
+    description: "This is a sample product",
+    image: "/client/images/medium-roast-coffee.jpg",
+    category: "mediumRoast",
+    price: 15.99,
+    quantity: 500
+  },
+  {
+    name: "Medium Roast 3",
+    description: "This is a sample product",
+    image: "/client/images/medium-roast-coffee.jpg",
+    category: "mediumRoast",
+    price: 15.99,
+    quantity: 500
+  },
+  {
+    name: "Dark Roast 1",
+    description: "This is a sample product",
+    image: "/client/images/dark-roast-coffee.jpg",
+    category: "darkRoast",
+    price: 16.99,
+    quantity: 500
+  },
+  {
+    name: "Dark Roast 2",
+    description: "This is a sample product",
+    image: "/client/images/dark-roast-coffee.jpg",
+    category: "darkRoast",
+    price: 16.99,
+    quantity: 500
+  },
+  
+  {
+    name: "Dark Roast 3",
+    description: "This is a sample product",
+    image: "/client/images/dark-roast-coffee.jpg",
+    category: "darkRoast",
+    price: 16.99,
+    quantity: 500
+  },
+  
+]
 // reserved for order seed
 // const orderSeedData = [
 //   {
@@ -52,7 +128,9 @@ db.once("open", async () => {
     await User.create(userSeedData);
     await Collection.deleteMany();
     await Collection.create(collectionSeedData);
-    console.log("Users and collections seeded successfully!");
+    await Product.deleteMany({});
+    await Product.insertMany(productSeedData);
+    console.log("Users, collections, & products seeded successfully!");
   } catch (err) {
     console.error(err);
   }
