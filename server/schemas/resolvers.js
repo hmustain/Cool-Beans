c31bd0ad2792b77e2c64f1458e5671821430537b
hmustain <hunter@bunkerbranding.com>
2023-03-24 08:53:31 -0500

    added resolvers and defined users in the resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,43 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find();
+    },
+
+    user: async (parent, { _id }) => {
+      return await User.findById(_id);
+    },
+
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findById(context.user._id);
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect email or password");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect email or password");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
