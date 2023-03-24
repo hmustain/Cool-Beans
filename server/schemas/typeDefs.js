b9ab3c0a068b9c7171e796ffda0067f2cd04ca6b
hmustain <hunter@bunkerbranding.com>
2023-03-24 08:58:06 -0500

    added typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express';
+
+const typeDefs = gql`
+type User {
+    _id: ID 
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    role: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    users: [User]
+    user(_id: ID!): User
+    me: User
+}
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
