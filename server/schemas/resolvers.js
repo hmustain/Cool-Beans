14a5e5d354632b0eb0b13dd626baee128b350809
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-24 12:01:03 -0400

    added query for collection model in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,12 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    categories: async () => {
+      return await Category.find();
+    },
     users: async () => {
       const users = await User.find();
       return users;
