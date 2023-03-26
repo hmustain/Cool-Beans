21874e538567bc2b589bc19ee3e44e9b9ed3f71f
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-25 20:43:50 -0400

    imported react and apollo server

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
