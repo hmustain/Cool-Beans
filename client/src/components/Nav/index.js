1cbeb482e0811590956dbd463833554bf6e1d627
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-27 18:34:36 -0400

    added navbar icons and imported bootstrap/bootstrap icons

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -27,21 +27,32 @@ function Nav() {
   //   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <ul>
-          <li>
-            <Link to="/home">Shop All</Link>
-          </li>
-          <li>
-            <Link to="/home">Cart</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/home">Single Serve</Link>
-          </li>
-        </ul>
-      </h1>
+    <header>
+      <div classname="header">
+        <div>
+          <h1>Coffee Brigade</h1>
+        </div>
+        <div>
+          <i class="bi bi-cart3"></i>
+          <i class="bi bi-person-circle"></i>
+        </div>
+      </div>
+      <nav className="navbar navbar-expand-lg custom-navbar">
+        <div className="collapse navbar-collapse " id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/home">Shop All</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/home">Coffee Bags</Link>
+            </li>
+            <li className="nav-item">
+              {" "}
+              <Link to="/home">Single Serve</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
       {/* <nav> {showNavigation()}</nav> */}
     </header>
