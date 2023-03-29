7e55f1c2a5448f4291ba118ce12542d616680d08
hmustain <hunter@bunkerbranding.com>
2023-03-29 14:52:00 -0500

    debugged issue in ShopAll.js where import was listed as ProductList instead of Products

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,63 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { pluralize } from "../../utils/helpers"
-// import { useStoreContext } from "../../utils/GlobalState";
-// import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-// import { idbPromise } from "../../utils/helpers";
+import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  // const [state, dispatch] = useStoreContext();
-console.log(item,"here");
+  console.log(item,"here");
+  const [state, dispatch] = useStoreContext();
   const {
     image,
     description,
     name,
     _id,
     price,
-    quantity,
-    category,
+    // quantity = 1,
+    // category = "coffee",
     reviews,
   } = item;
+  console.log('image', image);
+  console.log('description', description);
+  console.log('name', name);
+  console.log('_id', _id);
+  console.log('price', price);
+  // console.log('quantity', quantity);
+  // console.log('category', category);
+  console.log('reviews', reviews);
 
-  // const { cart } = state
+  const { cart } = state
 
-  // const addToCart = () => {
-  //   const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-  //   if (itemInCart) {
-  //     dispatch({
-  //       type: UPDATE_CART_QUANTITY,
-  //       _id: _id,
-  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-  //     });
-  //     idbPromise('cart', 'put', {
-  //       ...itemInCart,
-  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-  //     });
-  //   } else {
-  //     dispatch({
-  //       type: ADD_TO_CART,
-  //       product: { ...item, purchaseQuantity: 1 }
-  //     });
-  //     idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-  //   }
-  // }
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    }
+  }
 
   return (
     <div className="card px-1 py-1">
-      {/* <Link to={`/ProductItem/${_id}`}> */}
+      <Link to={`/ProductItem/${_id}`}>
         <img
           alt={name}
           src={`/images/${image}`}
         />
         <p>{name}</p>
-      {/* </Link> */}
+      </Link>
       <div>
-        <div>{quantity} {("item", quantity)} in stock</div>
+      {/* <div>{quantity} {pluralize("item", quantity)} in stock</div> */}
         <span>${price}</span><br></br>
         <span>${description}</span>
         <span>{displayAverageRating(reviews)}</span>
       </div>
-      <button >Add to cart</button>
+      <button onClick={addToCart} >Add to cart</button>
     </div>
   );
 }
