67a0c90c80ef0acb9152feb75f98c30784654d63
hmustain <hunter@bunkerbranding.com>
2023-03-29 11:52:57 -0500

    uncommented stuff out on productItem and added detail.js

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { pluralize } from "../../utils/helpers"
-// import { useStoreContext } from "../../utils/GlobalState";
-// import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-// import { idbPromise } from "../../utils/helpers";
+import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-//   const [state, dispatch] = useStoreContext();
+  const [state, dispatch] = useStoreContext();
 console.log(item,"here");
   const {
     image,
@@ -19,40 +19,40 @@ console.log(item,"here");
     reviews,
   } = item;
 
-//   const { cart } = state
+  const { cart } = state
 
-//   const addToCart = () => {
-//     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-//     if (itemInCart) {
-//       dispatch({
-//         type: UPDATE_CART_QUANTITY,
-//         _id: _id,
-//         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-//       });
-//       idbPromise('cart', 'put', {
-//         ...itemInCart,
-//         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-//       });
-//     } else {
-//       dispatch({
-//         type: ADD_TO_CART,
-//         product: { ...item, purchaseQuantity: 1 }
-//       });
-//       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-//     }
-//   }
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    }
+  }
 
   return (
     <div className="card px-1 py-1">
-      {/* <Link to={`/ProductItem/${_id}`}> */}
+      <Link to={`/ProductItem/${_id}`}>
         <img
           alt={name}
           src={`/images/${image}`}
         />
         <p>{name}</p>
-      {/* </Link> */}
+      </Link>
       <div>
-        <div>{quantity} {("item", quantity)} in stock</div>
+      <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span><br></br>
         <span>${description}</span>
         <span>{displayAverageRating(reviews)}</span>
