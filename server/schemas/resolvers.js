7b89feea273c585db30a28cf898415bf629ffa31
hmustain <hunter@bunkerbranding.com>
2023-03-30 23:49:48 -0500

    fixed a bug where stripe wasn't working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Category, Product, Review, Order } = require("../models");
 const { signToken } = require("../utils/auth");
-const stripe = require('stripe')('sk_test_51CsANGCxDRCVpUBx4jqN9xiGLqaq7l7EaH0uIdJAzi8Jrf33Fg3KyqCHCa8oTKMcAi9B0Iu9la62RedA2mKSrZXN00XWYaWuFM');
+const stripe = require("stripe")(
+"sk_test_51MrZYVJnjl6y4QyETfkbBV2ivhB2TnwWoCGCvuExq1EBfvvXaR8SaRT8ohJHmZo7jok6at08mzKbVIK3XIv3mRud00Ei9b3ndR"
+);
 
 console.log(process.env.STRIPE_SECRET_KEY);
 
@@ -20,27 +22,41 @@ const resolvers = {
         };
       }
 
-      const products = await Product.find(params).populate("category").select('-__v -updatedAt');
+      const products = await Product.find(params)
+        .populate("category")
+        .select("-__v -updatedAt");
 
       // Populate reviews for each product
       for (let i = 0; i < products.length; i++) {
-        products[i].reviews = await Review.find({ product: products[i]._id }).populate('user', '_id email firstName lastName');
+        products[i].reviews = await Review.find({
+          product: products[i]._id,
+        }).populate("user", "_id email firstName lastName");
       }
 
       return products;
     },
     product: async (parent, { _id }) => {
-      const product = await Product.findById(_id).populate("category").populate('reviews.user', '_id email firstName lastName');
-      product.reviews = await Review.find({ product: product._id }).populate('user', '_id email firstName lastName');
+      const product = await Product.findById(_id)
+        .populate("category")
+        .populate("reviews.user", "_id email firstName lastName");
+      product.reviews = await Review.find({ product: product._id }).populate(
+        "user",
+        "_id email firstName lastName"
+      );
       return product;
     },
     categories: async () => {
       return await Category.find();
     },
     reviews: async () => {
-      return await Review.find().populate('user', '_id email firstName lastName').populate({ path: 'product', populate: { path: 'reviews', populate: { path: 'createdAt' } } });
+      return await Review.find()
+        .populate("user", "_id email firstName lastName")
+        .populate({
+          path: "product",
+          populate: { path: "reviews", populate: { path: "createdAt" } },
+        });
     },
-    
+
     users: async () => {
       const users = await User.find();
       return users;
@@ -56,7 +72,7 @@ const resolvers = {
     order: async (parent, { _id }, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
-          path: "orders.product",
+          path: "orders.products",
           populate: "category",
         });
         const order = user.orders.id(_id);
@@ -66,39 +82,31 @@ const resolvers = {
     },
     checkout: async (parent, args, context) => {
       const url = new URL(context.headers.referer).origin;
-      const prods = await Product.find({
-        "_id": { $in: args.products }})
-      const order = new Order({ 
-        products: prods.map(product => {
-          return {
-            product: product._id,
-            quantity: product.quantity
-          }
-        }),
+      const order = new Order({
+        products: args.products,
       });
 
+      const { products } = await order.populate("products");
       const line_items = [];
-      const testorder = await order.populate("products");
-      console.log(testorder);
-      //const { products } = await order.populate("products");
-    //console.log("thais wants me to console log this", products);
-    
-    //   for (let i = 0; i < products.length; i++) {
-    //     const product = products[i].product;
-    
-    //     console.log('product', product);
-    //     const price = await stripe.prices.create({
-    //       product: product._id,
-    //       unit_amount:69,// products[i].price * 100,
-    //       currency: "usd",
-    //     });
-    // console.log('price right here ', price);
-    //     line_items.push({
-    //       price: price.id,
-    //       quantity: products[i].quantity,
-    //     });
-    //   }
-    console.log("console loggins is fun", line_items);
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+        })
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: "usd",
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      console.log("console loggins is fun", line_items);
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         line_items,
@@ -106,10 +114,10 @@ const resolvers = {
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`,
       });
-    
+
+
       return { session: session.id };
     },
-    
   },
 
   Mutation: {
@@ -121,10 +129,10 @@ const resolvers = {
     },
     addOrder: async (parent, args, context) => {
       if (context.user) {
-        const products = await Product.find({_id: {$in: args.products}});
-        console.log('here are the products', products);
+        const products = await Product.find({ _id: { $in: args.products } });
+        console.log("here are the products", products);
         const total = products.reduce(
-          (acc, { product, quantity }) => acc + (product.price * quantity),
+          (acc, { product, quantity }) => acc + product.price * quantity,
           0
         );
         const order = new Order({
@@ -133,9 +141,11 @@ const resolvers = {
           total,
           status: "confirmed",
         });
-        console.log('here is the new order', order);
+        console.log("here is the new order", order);
         await order.save();
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+        await User.findByIdAndUpdate(context.user._id, {
+          $push: { orders: order },
+        });
         return order;
       }
       throw new AuthenticationError("Not logged in");
@@ -165,13 +175,13 @@ const resolvers = {
       // Check if user has already reviewed the product
       const existingReview = await Review.findOne({
         user: context.user._id,
-        product: args._id
+        product: args._id,
       });
-    
+
       if (existingReview) {
         throw new Error("You've already reviewed this product!");
       }
-    
+
       // Create new review
       const product = await Product.findById(args._id);
       const newReview = await Review.create({
@@ -179,9 +189,9 @@ const resolvers = {
         comment: args.review.comment,
         createdAt: args.review.createdAt,
         user: context.user,
-        product
+        product,
       });
-    
+
       try {
         // Add review to product and save to database
         const updatedProduct = await Product.findOneAndUpdate(
@@ -192,14 +202,14 @@ const resolvers = {
           .populate("reviews")
           .populate({ path: "reviews", populate: "user" })
           .populate("category");
-    
+
         return updatedProduct;
       } catch (err) {
         console.log(err);
         throw new AuthenticationError("Invalid token");
       }
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
