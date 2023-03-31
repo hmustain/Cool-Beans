31b2108ed5d35483eca59bf60084d2f674c902d7
hmustain <hunter@bunkerbranding.com>
2023-03-31 14:25:39 -0500

    debugging CORS issue by installing package and adding it to server code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors'); 
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -16,6 +17,12 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:300',
+  credentials: true,
+}))
 
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/build/images')));
