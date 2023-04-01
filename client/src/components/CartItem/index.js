1399040cec637549f238e56a32717c8264968e28
lacnoskillz <kaikane3457@hotmail.com>
2023-03-31 22:15:11 -0500

    made style sheet for cartitem. now trashcan aja delete buton has clickable cursor on hover and the QTY doesnt have text cursor

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-
+import '../../styles/cartitem.css'
+import { Link } from 'react-router-dom'
 const CartItem = ({ item }) => {
 
   const [, dispatch] = useStoreContext();
@@ -39,15 +40,18 @@ const CartItem = ({ item }) => {
   return (
     <div className="flex-row">
       <div>
+        <Link to={`/Product/${item._id}`}>
+
         <img
           src={`/images/${item.image}`}
           alt=""
         />
+        </Link>
       </div>
       <div>
         <div>{item.name}, ${item.price}</div>
         <div>
-          <span>Qty:</span>
+          <span className='Qty'>Qty:</span>
           <input
             type="number"
             placeholder="1"
@@ -57,6 +61,7 @@ const CartItem = ({ item }) => {
           <span
             role="img"
             aria-label="trash"
+            className='trashcan'
             onClick={() => removeFromCart(item)}
           >
             🗑️
